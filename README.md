779af84340c099dca85de3f0355f5138001f1702
zx <zxcvbn@hrzn.email>
2024-02-09 19:31:01 -0500

    chore: add installer

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pyzule-gen
 a tool to generate .pyzule files
+
+# installation
+1. install [pyzule](https://github.com/asdfzxcvbn/pyzule?tab=readme-ov-file#installation)
+2. install `pyzule-gen`:
+   
+   `bash -c "$(curl https://raw.githubusercontent.com/asdfzxcvbn/pyzule-gen/main/install-pyzule-gen.sh`
---
+++ install-pyzule-gen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PZ_DIR=${HOME}/.config/pyzule
+
+if [ ! -d "${PZ_DIR}" ] || [ ! -x "$(command -v pyzule)" ]; then
+    echo "[!] pyzule is not installed"
+    exit 1
+fi
+
+echo "[*] installing pyzule-gen.."
+sudo curl -so /usr/local/bin/pyzule-gen https://raw.githubusercontent.com/asdfzxcvbn/pyzule-gen/main/pyzule-gen.py
+if [ "$OS" == "Linux" ]; then
+    sudo sed -i "1s|.*|#\!${PZ_DIR}/venv/bin/python|" /usr/local/bin/pyzule-gen
+else
+    sudo sed -e "1s|.*|#\!${PZ_DIR}/venv/bin/python|" -i "" /usr/local/bin/pyzule-gen  # bsd sed is broken asf
+fi
+echo "[*] fixed interpreter path!"
+sudo chmod +x /usr/local/bin/pyzule-gen
+echo "[*] done!"
