2288fd8bed77b038bc257e728e9bf0cec8f36e22
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2024-04-11 12:22:07 +0200

    pyzule is no dependency anymore

--- install-pyzule-gen.sh
+++ install-pyzule-gen.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 PZ_DIR=${HOME}/.config/pyzule
 
-echo "[*] installing pyzule-gen.."
+if [ ! -d "${PZ_DIR}" ]; then
+    echo "[*] ${PZ_DIR} does not exist. Creating..."
+    mkdir -p "${PZ_DIR}"
+fi
+
+if [ ! -d "${PZ_DIR}/venv" ]; then
+    echo "[*] Creating virtual environment..."
+    python3 -m venv "${PZ_DIR}/venv"
+fi
+
+source "${PZ_DIR}/venv/bin/activate"
+
+echo "[*] Installing dependencies..."
+pip install orjson Pillow &> /dev/null
+
+echo "[*] Installing pyzule-gen..."
 sudo curl -so /usr/local/bin/pyzule-gen https://raw.githubusercontent.com/asdfzxcvbn/pyzule-gen/main/pyzule-gen.py
+
 if [ "$(uname)" == "Linux" ]; then
     sudo sed -i "1s|.*|#\!${PZ_DIR}/venv/bin/python|" /usr/local/bin/pyzule-gen
 else
-    sudo sed -e "1s|.*|#\!${PZ_DIR}/venv/bin/python|" -i "" /usr/local/bin/pyzule-gen  # bsd sed is broken asf
+    sudo sed -e "1s|.*|#\!${PZ_DIR}/venv/bin/python|" -i "" /usr/local/bin/pyzule-gen
 fi
-echo "[*] fixed interpreter path!"
+
+echo "[*] Fixed interpreter path!"
 sudo chmod +x /usr/local/bin/pyzule-gen
-echo "[*] done!"
+
+echo "[*] pyzule-gen is ready to use!"
+echo "[*] Done!"
